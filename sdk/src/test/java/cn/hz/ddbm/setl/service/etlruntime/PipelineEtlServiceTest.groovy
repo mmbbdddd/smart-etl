b05cced67f99ad459603b360ff1447c8a378986c
mmbbdddd <81521544@qq.com>
2023-12-02 13:44:55 +0800

    调通基本逻辑

---
+++ sdk/src/test/java/cn/hz/ddbm/setl/service/etlruntime/PipelineEtlServiceTest.groovy
@@ -0,0 +1,43 @@
+package cn.hz.ddbm.setl.service.etlruntime;
+
+import cn.hz.ddbm.setl.EtlTestConfig;
+import cn.hz.ddbm.setl.config.EtlConfig
+import cn.hz.ddbm.setl.exception.EtlException
+import cn.hz.ddbm.setl.service.TaskFactory;
+import cn.hz.ddbm.setl.service.factory.PipelineFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Import([EtlTestConfig.class, EtlConfig.class])
+@ComponentScan("")
+public class PipelineEtlServiceTest {
+    @Autowired
+    PipelineFactory pipelineEtlService;
+
+    @Test
+    public void initWorkFlows() {
+        pipelineEtlService.initWorkFlows();
+    }
+
+    @Test
+    public void executeTask() {
+        try {
+            TaskFactory.EtlTaskResponse resp = pipelineEtlService.executeTask(new TaskFactory.EtlTaskRequest(
+                    taskCode: "etlTest",
+                    fluent: true
+            ));
+            println(resp.getTaskStatus())
+//            Assert.assertEquals(resp.getTaskStatus().equals(TaskStatus.ing),"")
+        } catch (EtlException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
